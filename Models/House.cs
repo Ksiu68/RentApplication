8ef1251c0b94c37a4a8475e7b285c221c42b84fc
oxugen <dubrovskay.7830@mail.ru>
2024-04-17 23:41:51 +0300

    fix house table

--- Models/House.cs
+++ Models/House.cs
@@ -15,6 +15,11 @@ namespace RentApplication.Models
         public string Interom { get; set; }
         public string Parking { get; set; }
         public string Playground { get; set; }
+        public string Area { get; set; }
+        public string NumberOfFloors { get; set; }
+        public string YearOfConstruction{get;set;}
+        public string Metro {get;set;}
+        public string DistanceToMetro{get;set;}
         public int NumberHouse { get; set; }
     }
 }
