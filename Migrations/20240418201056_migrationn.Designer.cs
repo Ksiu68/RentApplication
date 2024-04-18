441a32be83e6670fe350dfa9ef638c98236103a3
ksiu <ksyusha.dubrovskaya.01@mail.ru>
2024-04-18 23:22:20 +0300

    233

--- Migrations/20240418201056_migrationn.Designer.cs
+++ Migrations/20240418201056_migrationn.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RentApplication.Models;
 
 namespace RentApplication.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20240418201056_migrationn")]
+    partial class migrationn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20240418201056_migrationn.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RentApplication.Migrations
+{
+    public partial class migrationn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ImageAppartaments",
+                table: "ImageAppartaments");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_AppartamentAmeneties",
+                table: "AppartamentAmeneties");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AppartamentId",
+                table: "ImageAppartaments",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AppartamentId",
+                table: "AppartamentAmeneties",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ImageAppartaments",
+                table: "ImageAppartaments",
+                columns: new[] { "AppartamentId", "ImageId" });
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_AppartamentAmeneties",
+                table: "AppartamentAmeneties",
+                columns: new[] { "AppartamentId", "AmenetieId" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ImageAppartaments",
+                table: "ImageAppartaments");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_AppartamentAmeneties",
+                table: "AppartamentAmeneties");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AppartamentId",
+                table: "ImageAppartaments",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int))
+                .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AppartamentId",
+                table: "AppartamentAmeneties",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int))
+                .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ImageAppartaments",
+                table: "ImageAppartaments",
+                column: "AppartamentId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_AppartamentAmeneties",
+                table: "AppartamentAmeneties",
+                column: "AppartamentId");
+        }
+    }
+}
